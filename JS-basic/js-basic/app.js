128b5afbe05be5e51b3f39aa49bb60c7d62f7f66
evening-1 <evening-1@mstars-lab3-09s-iMac.local>
2022-11-28 17:58:12 +0800

    added js

---
+++ JS-basic/js-basic/app.js
@@ -0,0 +1,70 @@
+//Comments can make code readable
+//Javascript lesson begin
+//  * Comments can make code readable *
+
+//Variable.js
+
+let number=19;
+let string="ok";
+let boolean=false;
+// let undefined;
+// let null;
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+// console.log(undefined);
+// console.log(null);
+
+//Variable.js//
+
+//Datatypes.js
+
+console.log(typeof(number));
+console.log(typeof(string));
+console.log(typeof(boolean));
+// console.log(typeof(undefined));
+// console.log(typeof(null));
+
+//Datatypes.js//
+
+//task6,7
+
+let name;
+let surname="Josh";
+
+//task6,7//
+
+//task 8,9
+
+let firstname="";
+let lastname="";
+let marital_status="";
+let country="";
+let age=""
+
+let firstname1="" ; lastname1="" ; marital_status1="" ; country1="" ; age1="" 
+
+//task8,9//
+
+//task 10
+let myage=19;
+let yourage=20;
+
+console.log(myage);
+console.log(yourage);
+
+//task 10//
+
+//task11
+
+let x=3 , y=4 , z=10;
+console.log("x =" + x);
+console.log("y =" + y);
+console.log("z =" + z);
+
+
+
+
+
+
