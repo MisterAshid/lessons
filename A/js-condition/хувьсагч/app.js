3e716cc4d47767d310e962b589f76e14d15e7ec4
MisterAshid <116063794+MisterAshid@users.noreply.github.com>
2022-12-05 22:53:49 +0800

    added html

---
+++ A/js-condition/хувьсагч/app.js
@@ -0,0 +1,21 @@
+let a = 85, 
+b = 75, 
+c = 96, 
+d = 69;
+
+let sum = 0;
+
+if(a > 80){
+    sum = sum + a;
+}
+
+if(b > 80){
+    sum = sum + b
+}
+if(c > 80){
+    sum = sum + c
+}
+if(d > 80){
+    sum = sum + d
+}
+console.log("sum = ", sum)
